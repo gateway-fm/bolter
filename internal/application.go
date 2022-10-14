f8f735fac417b86c6abd3226102b1edf48f4fad3
Misnaged <pa100ni@gmail.com>
2022-10-14 15:19:07 +0300

    config fix

--- internal/application.go
+++ internal/application.go
@@ -32,6 +32,10 @@ type Bolter struct {
 func LoadBolter() error {
 
 	bolt := &Bolter{}
+	err := bolt.initCfg()
+	if err != nil {
+		return fmt.Errorf("unable to init bolter's cfg: %w", err)
+	}
 	file, err := bolt.CreateLoggerTextFile()
 	if err != nil {
 		return fmt.Errorf("CreateLoggerTextFile failed %w", err)
@@ -71,6 +75,7 @@ func LoadBolter() error {
 		}()
 		wg.Wait()
 	}
+
 	metrics.Close()
 
 	fmt.Printf("99th percentile: %s\n", metrics.Latencies.P99)
@@ -84,16 +89,18 @@ func (b *Bolter) DecodeConfig() (*config.BolterCfg, error) {
 	}
 	return &b.BolterCfg, nil
 }
-
-func (b *Bolter) NewBody() ([]byte, error) {
-	cfg, err := b.DecodeConfig()
+func (b *Bolter) initCfg() error {
+	_, err := b.DecodeConfig()
 	if err != nil {
-		return nil, fmt.Errorf("decoding failed: %w", err)
+		return fmt.Errorf("decoding failed: %w", err)
 	}
-	b.Request.Method = cfg.Request.Method
-	b.Request.Id = cfg.Request.Id
-	b.Request.Jsonrpc = cfg.Request.Jsonrpc
-	b.Request.Params = cfg.Request.Parameters
+	return nil
+}
+func (b *Bolter) NewBody() ([]byte, error) {
+	b.Request.Method = b.BolterCfg.Request.Method
+	b.Request.Id = b.BolterCfg.Request.Id
+	b.Request.Jsonrpc = b.BolterCfg.Request.Jsonrpc
+	b.Request.Params = b.BolterCfg.Request.Parameters
 	body, err := json.Marshal(b.Request)
 	if err != nil {
 		return nil, fmt.Errorf("marshalling failed: %w", err)
@@ -109,19 +116,15 @@ func (b *Bolter) PrepareAttacker() *vegeta.Attacker {
 
 // PrepareTargeter loading new vegeta.Targeter
 func (b *Bolter) PrepareTargeter() (vegeta.Targeter, error) {
-	cfg, err := b.DecodeConfig()
-	if err != nil {
-		return nil, fmt.Errorf("decoding failed: %w", err)
-	}
 
 	body, err := b.NewBody()
 	if err != nil {
 		return nil, fmt.Errorf("failed getting new body: %w", err)
 	}
-	b.Vegeta.Target.URL, b.Vegeta.Target.Method = cfg.Vegeta.Url, cfg.Vegeta.Method
+	b.Vegeta.Target.URL, b.Vegeta.Target.Method = b.BolterCfg.Vegeta.Url, b.BolterCfg.Vegeta.Method
 	b.Vegeta.Target.Body = body
-	if !cfg.Vegeta.IsPublic {
-		auth := cfg.Vegeta.Header.Auth + " " + cfg.Vegeta.Header.Bearer
+	if !b.BolterCfg.Vegeta.IsPublic {
+		auth := b.BolterCfg.Vegeta.Header.Auth + " " + b.BolterCfg.Vegeta.Header.Bearer
 		b.Vegeta.Target.Header = make(http.Header)
 		b.Vegeta.Target.Header.Add("Authorization", auth)
 	}
@@ -131,35 +134,23 @@ func (b *Bolter) PrepareTargeter() (vegeta.Targeter, error) {
 
 // PrepareRate loading new vegeta.Rate
 func (b *Bolter) PrepareRate() (*vegeta.Rate, error) {
-	cfg, err := b.DecodeConfig()
-	if err != nil {
-		return nil, fmt.Errorf("decoding failed: %w", err)
-	}
-	b.Vegeta.Rate.Freq = cfg.Vegeta.Rate
+	b.Vegeta.Rate.Freq = b.BolterCfg.Vegeta.Rate
 	b.Vegeta.Rate.Per = time.Second
 	return &b.Vegeta.Rate, nil
 }
 
 // PrepareDuration setting Duration for our attacker
 func (b *Bolter) PrepareDuration() (*time.Duration, error) {
-	cfg, err := b.DecodeConfig()
-	if err != nil {
-		return nil, fmt.Errorf("decoding failed: %w", err)
-	}
-	duration := time.Duration(cfg.Vegeta.Duration) * time.Second
+	duration := time.Duration(b.BolterCfg.Vegeta.Duration) * time.Second
 	b.Vegeta.Duration = duration
 	return &b.Vegeta.Duration, nil
 }
 
 func (b *Bolter) CreateLoggerTextFile() (*os.File, error) {
-	cfg, err := b.DecodeConfig()
-	if err != nil {
-		return nil, fmt.Errorf("decoding failed: %w", err)
-	}
-	b.Logger.LoggerType = cfg.Logger.LoggerType
+	b.Logger.LoggerType = b.BolterCfg.Logger.LoggerType
 
 	if b.Logger.LoggerType == 0 {
-		file, err := b.Logger.TextFile.CreateNewTextFile(cfg.Logger.FileName)
+		file, err := b.Logger.TextFile.CreateNewTextFile(b.BolterCfg.Logger.FileName)
 		if err != nil {
 			return nil, fmt.Errorf("creating new file failed: %w", err)
 		}
@@ -171,11 +162,8 @@ func (b *Bolter) CreateLoggerTextFile() (*os.File, error) {
 
 // PrepareLogger setting Duration for our attacker
 func (b *Bolter) PrepareLogger(data interface{}, file ...*os.File) error {
-	cfg, err := b.DecodeConfig()
-	if err != nil {
-		return fmt.Errorf("decoding failed: %w", err)
-	}
-	b.Logger.LoggerType = cfg.Logger.LoggerType
+	var err error
+	b.Logger.LoggerType = b.BolterCfg.Logger.LoggerType
 	switch b.Logger.LoggerType {
 	case 0:
 		b.Logger.TextFile.LogWithTextFile(data, file[0])
